13fb9e079cd9820c1af19247e766db04e2d457fa
delerueanthony5913-spec <delerue.anthony5913@gmail.com>
2026-05-05 13:21:30 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,1 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDJYT5L0A9f1YdRGEcvdk4iyoKgcfrBGWw",
+  authDomain: "immogerer-7f706.firebaseapp.com",
+  projectId: "immogerer-7f706",
+  storageBucket: "immogerer-7f706.firebasestorage.app",
+  messagingSenderId: "703084929054",
+  appId: "1:703084929054:web:313fae5f706e4dba4fce0f",
+  measurementId: "G-QQE1Q309TB"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const appId = 'immogerer-prod-final';
